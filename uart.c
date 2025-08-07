70b83215b244c55c800dafe59c62d34521021fee
LauHaven <Havenslau@hotmail.com>
2025-08-07 22:48:28 +0800

    dual port initial

--- uart.c
+++ uart.c
@@ -6,7 +6,13 @@ void uart(void)
     printf("print information to uart port");
 }
 
+<<<<<<< HEAD
 void single_uart_init(void)
 {
     printf("single port");
+=======
+void dual_port_initial(void)
+{
+    printf("dual port initial");
+>>>>>>> 420981c (dual port initial)
 }
